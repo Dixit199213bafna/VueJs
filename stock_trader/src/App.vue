1654c11490dc14935cf757e24268c0925d78df22
Dixit R Bafna <dixitrbafna@Dixits-MacBook-Pro.local>
2020-04-19 15:48:10 +0200

    Added components and router configuration

---
+++ stock_trader/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container">
+    <app-header></app-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ stock_trader/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Stock Trade</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link tag="li" to="/" active-class="nav-item active"><a class="nav-link">Home</a></router-link>
+        <router-link tag="li" to="/portfolio" active-class="nav-item active"><a class="nav-link">Portfolio</a></router-link>
+        <router-link tag="li" to="/stocks" active-class="nav-item active"><a class="nav-link">Stocks</a></router-link>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">End Day</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Save & load
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Save Data</a>
+              <a class="dropdown-item" href="#">Load Data</a>
+            </div>
+          </li>
+        </ul>
+      </form>
+    </div>
+  </nav>
+</template>
